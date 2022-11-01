50b7ced2b93b96a8fb02253f3094456603f4d5eb
OrangeKir <fraiming_king@outlook.com>
2022-11-02 00:36:16 +0300

    Add basic registrations

--- OkArt/Program.cs
+++ OkArt/Program.cs
@@ -1,4 +1,10 @@
+using System.Data;
+using MediatR;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
+var type = typeof(Program);
+var dbConnectionString = builder.Configuration.GetConnectionString("DbConnectionString");
 
 // Add services to the container.
 
@@ -6,6 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(type);
+builder.Services.AddScoped<IDbConnection>(_ => new NpgsqlConnection(dbConnectionString));
 
 var app = builder.Build();
 
